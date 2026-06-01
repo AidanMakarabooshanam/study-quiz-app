2c6f9cd8c7e8ba1345b0aca02bd198ee73a20e1a
aidan-makarabooshanam <aidan.m0c@stu.austinisd.org>
2026-05-31 20:16:29 -0500

    add question creation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -7,7 +7,18 @@ questions = []
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", questions=questions)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        questions.append({
+            "question": request.form["question"],
+            "answer": request.form["answer"],
+        })
+        return redirect(url_for("index"))
+    return render_template("add.html")
 
 
 if __name__ == "__main__":
